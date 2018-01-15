4b6372b6a0d33f3c7878ae6216b4ba32de5557c2
Sidharth D <sidharth363@gmail.com>
2018-01-15 10:56:25 +0530

    Tweets in state

--- App.js
+++ App.js
@@ -5,6 +5,42 @@ import TweetPreview from './Components/TweetPreview.js';
 
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweets: [
+        {
+          key: 1,
+          tweet: 'Hello CUSAT! I am Sidharth, here at CUSAT to introduce you guys to the super cool technology React Native. Trust me, I am excited to be here :)',
+          author: 'Sidharth',
+          handle: 'sidharth',
+          image: 'http://placeimg.com/50/50/people/grayscale',
+        },
+        {
+          key: 2,
+          tweet: 'You either die a hero, or live long enough to see yourself become the villian.',
+          author: 'Harvey',
+          handle: 'harvey',
+          image: 'http://placeimg.com/50/50/people/grayscale',
+        },
+        {
+          key: 3,
+          tweet: "You don't seize the moment; the moment seizes you",
+          author: 'The boy',
+          handle: 'theboy',
+          image: 'http://placeimg.com/50/50/people/grayscale',
+        },
+        {
+          key: 4,
+          tweet: 'Hello CUSAT! I am Sidharth, here at CUSAT to introduce you guys to the super cool technology React Native. Trust me, I am excited to be here :)',
+          author: 'Sidharth',
+          handle: 'sidharth',
+          image: 'http://placeimg.com/50/50/people/grayscale',
+        },
+      ]
+    }
+  }
+
   renderItem = ({item}) => {
     return(
       <TweetPreview text={item.tweet} author={item.author} image={item.image} />
@@ -12,76 +48,11 @@ export default class App extends Component {
   }
 
   render() {
-    const tweets = [
-      {
-        key: 1,
-        tweet: 'Hello CUSAT! I am Sidharth, here at CUSAT to introduce you guys to the super cool technology React Native. Trust me, I am excited to be here :)',
-        author: 'Sidharth',
-        handle: 'sidharth',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      },
-      {
-        key: 2,
-        tweet: 'You either die a hero, or live long enough to see yourself become the villian.',
-        author: 'Harvey',
-        handle: 'harvey',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      },
-      {
-        key: 3,
-        tweet: "You don't seize the moment; the moment seizes you",
-        author: 'The boy',
-        handle: 'theboy',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      },
-      {
-        key: 4,
-        tweet: 'Hello CUSAT! I am Sidharth, here at CUSAT to introduce you guys to the super cool technology React Native. Trust me, I am excited to be here :)',
-        author: 'Sidharth',
-        handle: 'sidharth',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      },
-      {
-        key: 5,
-        tweet: 'You either die a hero, or live long enough to see yourself become the villian.',
-        author: 'Harvey',
-        handle: 'harvey',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      },
-      {
-        key: 6,
-        tweet: "You don't seize the moment; the moment seizes you",
-        author: 'The boy',
-        handle: 'theboy',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      },
-      {
-        key: 7,
-        tweet: 'Hello CUSAT! I am Sidharth, here at CUSAT to introduce you guys to the super cool technology React Native. Trust me, I am excited to be here :)',
-        author: 'Sidharth',
-        handle: 'sidharth',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      },
-      {
-        key: 8,
-        tweet: 'You either die a hero, or live long enough to see yourself become the villian.',
-        author: 'Harvey',
-        handle: 'harvey',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      },
-      {
-        key: 9,
-        tweet: "You don't seize the moment; the moment seizes you",
-        author: 'The boy',
-        handle: 'theboy',
-        image: 'http://placeimg.com/50/50/people/grayscale',
-      }
-    ]
 
     return(
       <View style={styles.container}>
         <FlatList
-          data={tweets}
+          data={this.state.tweets}
           renderItem={this.renderItem}
         />
       </View>
